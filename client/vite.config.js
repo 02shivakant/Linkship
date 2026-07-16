a24e46fab10a6d2cb43945d76608d27fb80f676a
dearravan02 <sanjaypandey2334@gmail.com>
2026-07-17 03:37:15 +0530

    Fix: import.meta does not work in vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,17 +1,23 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
-const API_URL = import.meta.env.VITE_API_URL;
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: API_URL || 'http://localhost:5000',
-        changeOrigin: true,
+  console.log("Mode:", mode);
+  console.log("VITE_API_URL:", env.VITE_API_URL);
+
+  return {
+    plugins: [react()],
+    server: {
+      port: 5173,
+      proxy: {
+        "/api": {
+          target: env.VITE_API_URL || "http://localhost:5000",
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
+  };
 });
